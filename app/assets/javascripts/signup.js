ded86e9410d31f61b85471d6c14d2619adbce0f6
Roger Graves <rogergraves@gmail.com>
2013-01-22 22:38:50 -0800

    Get carousel working properly (without form submissions)

--- app/assets/javascripts/signup.js
+++ app/assets/javascripts/signup.js
@@ -1,13 +1,30 @@
 $(document).ready(function() {
-   var activateSecondSlide = function() {
-       console.log("We're in!");
-       if($('#respondent_event').val() && $('#respondent_number_of_donors').val()) {
-//            $('.carousel-control.right').show();
-       }
-   }
+    $('.carousel').carousel({
+        interval: false
+    })
 
-   $('#respondent_number_of_donors').change(activateSecondSlide);
-   $('#respondent_event').change(activateSecondSlide);
+    var activateSecondSlide = function() {
+       if($('#respondent_event').val() && $('#respondent_number_of_donors').val()) {
+            $('.carousel-control.right').show();
+       }
+    }
+
+    var submitFinalSlide = function() {
+        $('.carousel-control.right').show();
+    }
+
+    // First slide listeners
+    $('#respondent_number_of_donors').change(activateSecondSlide);
+    $('#respondent_event').change(activateSecondSlide);
     $('#respondent_number_of_donors').keypress(activateSecondSlide);
     $('#respondent_event').keypress(activateSecondSlide);
+
+    // Second slide listeners
+    $('#respondent_email').keypress(submitFinalSlide);
+
+
+    $('.carousel-control.right').click(function() {
+        $('.carousel-control.right').hide();
+    })
+
 });
