24fff08d2c057e39488d7421bc2d17bddaf848dc
Ben Johnston <benjohnstonsf@gmail.com>
2013-01-28 20:12:03 -0800

    Added favicon

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130122072248) do
+ActiveRecord::Schema.define(:version => 20130122071702) do
 
   create_table "respondents", :force => true do |t|
     t.string   "email"
