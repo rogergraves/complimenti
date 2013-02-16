3206c2c30ef6ed8cd81c5252e85412333ac4a776
Roger Graves <rogergraves@gmail.com>
2013-02-15 17:31:59 -0800

    Add tree growing animation on slide 2

--- app/assets/javascripts/signup.js
+++ app/assets/javascripts/signup.js
@@ -13,7 +13,6 @@ $(document).ready(function() {
     }
 
     var makeButtonVisibleOfFocus = function() {
-        console.log("makeButtonVisibleOfFocus called, event_type: "+$("#event_type").val()+" and number_of_donors: ", $('#number_of_donors').val());
         if($("#event_type").val() && !$('#number_of_donors').val()) {
             $('#number_of_donors').focus();
         } else {
@@ -39,6 +38,7 @@ $(document).ready(function() {
     $("#button_1").click(function() {
         $("#carousel-inner").removeClass("red-border");
         $("#carousel-inner").addClass("green-border");
+        growTree();
     });
     $("#button_2").click(function() {
         $("#carousel-inner").removeClass("green-border");
@@ -51,5 +51,40 @@ $(document).ready(function() {
         $("#carousel-inner").removeClass("red-border");
         $("#carousel-inner").addClass("green-border");
     });
+
+
+    var swapImage = function(variations, timing) {
+        timing = typeof timing !== 'undefined' ? timing : 4000;
+
+        var variation = variations.pop();
+
+        var image = $('.tree');
+
+        setTimeout(function() {
+            image.fadeOut(1000, function () {
+                image.css("background", "url('/assets/tree_"+variation+".png')");
+                image.fadeIn(1000);
+            });
+
+            if(variations.length > 0) {
+                swapImage(variations);
+            } else {
+                setTimeout(function() {
+                    $('.bubble-1').show();
+                }, 3000);
+                setTimeout(function() {
+                    $('.bubble-2').show();
+                }, 4000);
+                setTimeout(function() {
+                    $('.bubble-3').show();
+                }, 5000);
+            }
+        }, timing);
+    };
+
+    var growTree = function() {
+        var variations = ['large', 'medium', 'small'];
+        swapImage(variations, 500);
+    };
 
 });
